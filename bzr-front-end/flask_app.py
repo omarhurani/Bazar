c3b859474dbb3ad215645ceaa1999565030a0576
omarhurani <omarhurani@gmail.com>
2020-11-11 01:12:35 +0200

    Initialize front end server

---
+++ bzr-front-end/flask_app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from os import environ
+
+app = Flask(__name__)
+
+CATALOG_ADDRESS = environ.get('CATALOG_ADDRESS')
+ORDER_ADDRESS = environ.get('ORDER_ADDRESS')
+
+
+app.config['FLASK_ENV'] = environ.get('FLASK_ENV')
+app.config['FLASK_DEBUG'] = bool(environ.get('FLASK_DEBUG'))
