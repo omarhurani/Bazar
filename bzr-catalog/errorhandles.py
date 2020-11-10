8bf4ee5c16cd12247042e60840738ba992134123
omarhurani <omarhurani@gmail.com>
2020-11-10 13:07:59 +0200

    Add HTTP response status

--- bzr-catalog/errorhandles.py
+++ bzr-catalog/errorhandles.py
@@ -4,10 +4,10 @@ from flask_app import app
 @app.errorhandler(404)
 def not_found(e):
     return {'message': 'The requested URL was not found on the server. If you entered the URL manually please check '
-                       'your spelling and try again.'}
+                       'your spelling and try again.'}, 404
 
 
 @app.errorhandler(500)
 def internal_server_error(e):
     return {'message': 'The server encountered an internal error and was unable to complete your request. Either the '
-                       'server is overloaded or there is an error in the application.'}
+                       'server is overloaded or there is an error in the application.'}, 500
