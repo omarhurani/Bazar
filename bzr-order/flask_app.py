1fb47630a49d47d2b8205f089b1943fb42a183f6
omarhurani <omarhurani@gmail.com>
2020-11-13 03:08:10 +0200

    Add comments to order server

--- bzr-order/flask_app.py
+++ bzr-order/flask_app.py
@@ -1,13 +1,17 @@
 from flask import Flask
 from os import environ
 
+# Flask application instance
 app = Flask(__name__)
 
+# Get addresses of catalog and order servers from the environment variables
 CATALOG_ADDRESS = environ.get('CATALOG_ADDRESS')
 FRONT_END_ADDRESS = environ.get('FRONT_END_ADDRESS')
 
 
+# Get the flask environment settings from the environment variables
 app.config['FLASK_ENV'] = environ.get('FLASK_ENV')
 app.config['FLASK_DEBUG'] = bool(environ.get('FLASK_DEBUG'))
 
+# Get the application port from the environment variables
 port = int(environ.get('FLASK_PORT'))
