87239dbde8d66971d4c9219310eb70de01f0a0ef
omarhurani <omarhurani@gmail.com>
2020-11-13 02:42:48 +0200

    Use query and update endpoints in order server

--- bzr-order/routes.py
+++ bzr-order/routes.py
@@ -7,8 +7,16 @@ import requests
 def buy(book_id):
     if not book_id.isnumeric():
         return {'message': 'Book ID must be a number'}, 422
-    book_request = requests.get(f'{CATALOG_ADDRESS}/lookup/{book_id}')
-    if book_request.status_code != 200:
-        return book_request.content, book_request.status_code, book_request.headers.items()
-    buy_request = requests.put(f'{CATALOG_ADDRESS}/buy/{book_id}')
-    return buy_request.text, buy_request.status_code, buy_request.headers.items()
+    book_response = requests.get(f'{CATALOG_ADDRESS}/query/item/{book_id}')
+    if book_response.status_code == 404:
+        return {'message': 'Book with the specified ID does not exist'}, 404
+    elif book_response.status_code != 200:
+        return book_response.content, book_response.status_code, book_response.headers.items()
+    book = book_response.json()
+    if book['quantity'] <= 0:
+        return {'success': False, 'message': 'Book with the specified ID is out of stock'}
+    buy_response = requests.put(f'{CATALOG_ADDRESS}/update/{book_id}', json={'quantity': book['quantity']-1})
+    if buy_response.status_code != 200:
+        return buy_response.text, buy_response.status_code, buy_response.headers.items()
+    return {'success': True, 'message': 'Book with the specified ID purchased'}
+
